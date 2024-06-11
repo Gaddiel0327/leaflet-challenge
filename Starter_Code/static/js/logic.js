363039a03f05291fcb8f08cc7e74e27bf794b517
Gaddiel <gaddielhernandez10@gmail.com>
2024-06-10 23:31:10 -0500

    all commit

---
+++ Starter_Code/static/js/logic.js
@@ -0,0 +1,12 @@
+{
+    "type": "FeatureCollection",
+    "metadata": {
+        "generated": 1718079597000,
+        "url": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson",
+        "title": "USGS All Earthquakes",
+        "api": "1.10.3",
+        "count": 2139,
+        "status": 200
+    }
+}
+
